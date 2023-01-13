cdc173cac7e3da1bfeb1608cf096fb2324d2794f
juan.penagos <juan.penagos.baez@udla.edu.ec>
2023-01-13 15:25:10 -0500

    Inicio

--- main.c
+++ main.c
@@ -11,22 +11,26 @@ int main()
     scanf("%lf",&n1);
     printf("INGRESE EL SEGUNDO NUMERO: ");
     scanf("%lf",&n2);
-    
+    suma(n1,n2);
+    resta(n1,n2);
+    multiplicacion(n1,n2);
+    division(n1,n2);
+
     return 0;
 }
 void suma (double a,double b){
     double s=a+b;
-    printf("La suma es: %lf",s);
+    printf("La suma es: %lf\n\n",s);
 }
 void resta (double a,double b){
     double s=a-b;
-    printf("La resta es: %lf",s);
+    printf("La resta es: %lf ",s);
 }
 void multiplicacion (double a,double b){
     double s=a*b;
-    printf("La multiplicacion es: %lf",s);
+    printf("La multiplicacion es: \n\n%lf",s);
 }
 void division (double a,double b){
     double s=a/b;
-    printf("La division es: %lf",s);
+    printf("La division es: \n\n%lf",s);
 }
